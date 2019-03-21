efdfff4c3f02e19b2eebf111415a46bad69c5984
Leszek Błażewski <blazewski.leszek@gmail.com>
2019-03-21 23:37:40 +0100

    Fixed all database related operations, updated tests, all data is pulled correctly and ready to be put into reports.

--- ReportGenerator.UnitTests/DatabaseTests.cs
+++ ReportGenerator.UnitTests/DatabaseTests.cs
@@ -270,14 +270,14 @@ namespace ReportGenerator.UnitTests
         {
             //Arrange
             var viewModel = new OrderViewModel();
-            decimal result = 2.5m;
+            decimal result = 4m;
 
             // Add two orders to database
             for (int i = 1; i < 3; i++)
             {
                 viewModel.AddNewOrder(new Order
                 {
-                    ClientId = "id",
+                    ClientId = "id" + i,
                     RequestId = i,
                     Name = "Roll",
                     Quantity = i,
@@ -285,24 +285,37 @@ namespace ReportGenerator.UnitTests
                 });
             }
 
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id"+1,
+                RequestId = 1,
+                Name = "Roll",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+
             //Action
-            decimal totalPriceOfOrdersForClient = viewModel.GetTotalPriceOfOrdersForSpecificClient("");
+            decimal totalPriceOfOrdersForClient = viewModel.GetTotalPriceOfOrdersForSpecificClient("id1");
            
             //Assert
             Assert.AreEqual(result, totalPriceOfOrdersForClient);
         }
 
         [TestMethod]
-        public void test()
+        public void GetAveragePriceOfOrder_OrdersExistInDatabase_AveragePriceIsReturned()
         {
-            OrderViewModel viewModel = new OrderViewModel();
+            //Arrange
+            var viewModel = new OrderViewModel();
+            decimal average = 5.5m;
 
             // Add two orders to database
             for (int i = 1; i < 3; i++)
             {
                 viewModel.AddNewOrder(new Order
                 {
-                    ClientId = "id",
+                    ClientId = "id" + i,
                     RequestId = i,
                     Name = "Roll",
                     Quantity = i,
@@ -310,7 +323,180 @@ namespace ReportGenerator.UnitTests
                 });
             }
 
-            var orders = viewModel.test();
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id" + 1,
+                RequestId = 1,
+                Name = "Roll",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+
+            //Action
+            decimal averagePrice = viewModel.GetAveragePriceOfOrder();
+
+            //Assert
+            Assert.AreEqual(average, averagePrice);
+        }
+
+        [TestMethod]
+        public void GetAveragePriceOfOrderOfSpecificClient_OrdersExistInDatabase_AveragePriceIsReturned()
+        {
+            //Arrange
+            var viewModel = new OrderViewModel();
+            decimal average = 2.75m;
+
+            // Add two orders to database
+            for (int i = 1; i < 3; i++)
+            {
+                viewModel.AddNewOrder(new Order
+                {
+                    ClientId = "id" + i,
+                    RequestId = i,
+                    Name = "Roll",
+                    Quantity = i,
+                    Price = 1.5m + i,
+                });
+            }
+
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id" + 1,
+                RequestId = 1,
+                Name = "Roll",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id" + 1,
+                RequestId = 2,
+                Name = "Roll",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+            //Action
+            decimal averagePrice = viewModel.GetAveragePriceOfOrderOfSpecificClient("id1");
+
+            //Assert
+            Assert.AreEqual(average, averagePrice);
+        }
+
+
+        [TestMethod]
+        public void GetNumberOfRequestsGroupedByName_OrdersExistInDatabase_GrouppedRequestsAreReturned()
+        {
+            //Arrange
+            var viewModel = new OrderViewModel();
+           
+            // Add two orders to database
+            for (int i = 1; i < 3; i++)
+            {
+                viewModel.AddNewOrder(new Order
+                {
+                    ClientId = "id" + i,
+                    RequestId = i,
+                    Name = "Roll",
+                    Quantity = i,
+                    Price = 1.5m + i,
+                });
+            }
+
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id" + 1,
+                RequestId = 1,
+                Name = "Stuck",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id" + 1,
+                RequestId = 2,
+                Name = "Car",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+            var groupedOrders = viewModel.GetNumberOfOrdersGroupedByName();
+
+            //Assert
+            // only 3 orders should be returned because two orders referee to same name
+            Assert.AreEqual(3, groupedOrders.Count);
+        }
+
+        [TestMethod]
+        public void GetNumberOfRequestsGroupedByNameForSpecificClient_OrdersExistInDatabase_GrouppedRequestsAreReturned()
+        {
+            //Arrange
+            var viewModel = new OrderViewModel();
+
+            // Add two orders to database
+            for (int i = 1; i < 3; i++)
+            {
+                viewModel.AddNewOrder(new Order
+                {
+                    ClientId = "id1",
+                    RequestId = i,
+                    Name = "Roll",
+                    Quantity = i,
+                    Price = 1.5m + i,
+                });
+            }
+
+            // add order with same request id  && client id to check if data is aggregated correctly
+            viewModel.AddNewOrder(new Order
+            {
+                ClientId = "id1",
+                RequestId = 1,
+                Name = "Car",
+                Quantity = 1,
+                Price = 1.5m,
+            });
+
+            var groupedOrdersForClient = viewModel.GetNumberOfOrdersGroupedByNameForSpecificClient("id1");
+
+            // Assert
+            // Only 2 orders should be returned because two orders referee to same name
+            Assert.AreEqual(2, groupedOrdersForClient.Count);
+        }
+
+        [TestMethod]
+        public void GetOrdersInGivenRange_OrdersExistInDatabase_OrdersInGivenRangeAreReturned()
+        {
+            //Arrange
+            var viewModel = new OrderViewModel();
+            decimal lowerBound = 1.5m;
+            decimal upperBound = 5.7m;
+
+            // Add two orders to database
+            for (int i = 1; i < 3; i++)
+            {
+                viewModel.AddNewOrder(new Order
+                {
+                    ClientId = "id1",
+                    RequestId = i,
+                    Name = "Roll",
+                    Quantity = i,
+                    Price = 1.5m + i,
+                });
+            }
+
+            var ordersInGivenRange = viewModel.GetOrdersInGivenRange(lowerBound, upperBound);
+
+            // Assert
+            // Orders in range( price >= lowerBound && price <= upperBound) are returned
+            ordersInGivenRange.ForEach(order => Assert.IsTrue(order.Price >= lowerBound && order.Price <= upperBound));
         }
     }
 }
--- ReportGenerator/ViewModel/OrderViewModel.cs
+++ ReportGenerator/ViewModel/OrderViewModel.cs
@@ -10,6 +10,7 @@ namespace ReportGenerator.ViewModel
         private readonly OrdersDatabase database = new OrdersDatabase();
 
         //TODO: ** THIS FUNCTION MIGHT BE USED TO INSERT ROW INTO DATABASE DIRECTLY FROM THE APP NOT FROM PARSERS**
+        // ALSO used for testing purpouses 
         public void AddNewOrder(Order order)
         {
             using (TransactionScope tran = new TransactionScope())
@@ -38,11 +39,10 @@ namespace ReportGenerator.ViewModel
             return orders;
         }
 
-        // TODO FIX THIS 
         public List<Order> GetAllOrdersForSpecificClient(string clientId)
         {
             var clientOrders = database.Orders
-                                      .Select(x => new Order { RequestId = x.RequestId, ClientId = x.ClientId, Price = (x.Price * x.Quantity) })
+                                      .Select(x => new Order { RequestId = x.RequestId, ClientId = x.ClientId.TrimEnd(), Price = (x.Price * x.Quantity) })
                                       .Where(z => z.ClientId == clientId)
                                       .GroupBy(order => order.RequestId)
                                       .Select(g => new Order { RequestId = g.Key, Price = g.Sum(x => x.Price) })
@@ -59,11 +59,10 @@ namespace ReportGenerator.ViewModel
                                  .Count();
         }
 
-        //TODO FIX THIS 
         public int GetNumberOfOrdersForSpecificClient(string clientId)
         {
             return database.Orders
-                                 .Select(x => new Order { RequestId = x.RequestId, ClientId = x.ClientId })
+                                 .Select(x => new Order { RequestId = x.RequestId, ClientId = x.ClientId.TrimEnd() })
                                  .Where(z => z.ClientId == clientId)
                                  .GroupBy(order => order.RequestId)
                                  .Select(g => g.Key)
@@ -79,19 +78,18 @@ namespace ReportGenerator.ViewModel
             return totalPrice;
         }
 
-        //TODO: CHECK IF THIS WORKS
         public decimal GetTotalPriceOfOrdersForSpecificClient(string clientId)
         {
             var totalPriceOfOrdersForClient = database.Orders
-                                                             .Select(x => new Order { RequestId = x.RequestId,Price = (x.Price * x.Quantity) }).ToList();
-                                                             //.Where(z => z.ClientId == clientId).Count();
-                                                             //.GroupBy(order => order.RequestId)
-                                                             //.Sum(x => x.Sum(y => y.Price));
+                                                             .Select(x => new Order { RequestId = x.RequestId,ClientId = x.ClientId.TrimEnd(), Price = (x.Price * x.Quantity)})
+                                                             .Where(z => z.ClientId == clientId)
+                                                             .GroupBy(order => order.RequestId)
+                                                             .Sum(x => x.Sum(y => y.Price));
 
-            return 0;
+            return totalPriceOfOrdersForClient;
         }
 
-        //TODO: CHECK IF WORKS
+ 
         public decimal GetAveragePriceOfOrder()
         {
             decimal averagePriceOfOrder = database.Orders
@@ -102,59 +100,49 @@ namespace ReportGenerator.ViewModel
             return averagePriceOfOrder;
         }
 
-        //TODO:CHECK IF WORKS
         public decimal GetAveragePriceOfOrderOfSpecificClient(string clientId)
         {
             decimal averagePriceOfOrder = database.Orders
-                                                       .Select(x => new Order { RequestId = x.RequestId, Price = (x.Price * x.Quantity) })
+                                                       .Select(x => new Order { RequestId = x.RequestId, ClientId = x.ClientId.TrimEnd(), Price = (x.Price * x.Quantity) })
                                                        .Where(order => order.ClientId == clientId)
                                                        .GroupBy(order => order.RequestId)
                                                        .Average(x => x.Sum(y => y.Price));
             return averagePriceOfOrder;
         }
 
-        //TODO:CHECK IF WORKS
-        public List<Order> GetNumberOfRequestsGroupedByName()
+        public List<Order> GetNumberOfOrdersGroupedByName()
         {
             var orders = database.Orders
-                                        .Select(x => new Order { RequestId = x.RequestId, Name = x.Name, Quantity = x.Quantity })
-                                        .GroupBy(order => new { order.RequestId, order.Name, })
-                                        .Select(g => new Order { Name = g.Key.Name, Quantity = g.Sum(z => z.Quantity) })
+                                        .Select(x => new Order { RequestId = x.RequestId, Name = x.Name.TrimEnd(), Quantity = x.Quantity })
+                                        .GroupBy(order => order.Name )
+                                        .Select(g => new Order { Name = g.Key, Quantity = g.Sum(z => z.Quantity) }) 
                                         .ToList();
 
             return orders;
         }
 
-        //TODO:CHECK IF WORKS
-        public List<Order> GetNumberOfRequestsGroupedByNameForSpecificClient(string clientId)
+        public List<Order> GetNumberOfOrdersGroupedByNameForSpecificClient(string clientId)
         {
             var clientOrders = database.Orders
-                                        .Select(x => new Order { RequestId = x.RequestId, Name = x.Name, Quantity = x.Quantity })
-                                        .Where(order => order.ClientId == clientId)
-                                        .GroupBy(order => new { order.RequestId, order.Name, })
-                                        .Select(g => new Order { Name = g.Key.Name, Quantity = g.Sum(z => z.Quantity) })
-                                        .ToList();
+                                       .Select(x => new Order { RequestId = x.RequestId, Name = x.Name.TrimEnd(), Quantity = x.Quantity, ClientId = x.ClientId.TrimEnd() })
+                                       .Where(z => z.ClientId == clientId)
+                                       .GroupBy(order => order.Name)
+                                       .Select(g => new Order { Name = g.Key, Quantity = g.Sum(z => z.Quantity) })
+                                       .ToList();
 
             return clientOrders;
         }
 
-        //TODO:CHECK IF WORKS
-        public List<Order> GetNumberOfRequestsGroupedByNameForSpecificClient(decimal lowerBound, decimal upperBound)
+        public List<Order> GetOrdersInGivenRange(decimal lowerBound, decimal upperBound)
         {
             var ordersInGivenPriceInterval = database.Orders
                                                             .Select(x => new Order { RequestId = x.RequestId, Price = (x.Price * x.Quantity) })
-                                                            .Where(z => z.Price >= lowerBound && z.Price <= upperBound)
                                                             .GroupBy(order => order.RequestId)
                                                             .Select(g => new Order { RequestId = g.Key, Price = g.Sum(x => x.Price) })
+                                                            .Where(z => z.Price >= lowerBound && z.Price <= upperBound)
                                                             .ToList();
 
             return ordersInGivenPriceInterval;
-        }
-
-        public List<Order> test()
-        {
-            var orders = database.Orders.ToList();
-            return orders;
         }
     }
 }
